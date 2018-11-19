77286e0c36fc399ce990afd8a6228803630acfe7
Enrique <enrique.alcaide92@gmail.com>
2018-11-18 18:49:54 -0800

    Execute the main

--- src/Driver.java
+++ src/Driver.java
@@ -1,7 +1,19 @@
 
 public class Driver {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
+        DLPriorityQueue DLPrioQue = new DLPriorityQueue();
 
-  }
+        // Insert data in the Queue
+        DLPrioQue.enqueue(0, "Cero");
+        DLPrioQue.enqueue(1, "One");
+        DLPrioQue.enqueue(2, "Two");
+        DLPrioQue.enqueue(3, "Three");
+        DLPrioQue.enqueue(4, "Four");
+
+        // Print the Queue --> DLPrioQue
+        for (DLPrioQue queue : DLPrioQue) {
+            System.out.println(DLPrioQue.getQueue());
+        }
+    }
 }
